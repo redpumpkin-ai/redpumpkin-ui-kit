47f7113a98097d5a6b71ebe803f3b7ff3cdf38b2
Fachri Riyanto <fachri@redpumpkin.ai>
2026-01-30 11:12:26 +0700

    update: component token usage

---
+++ lib/components/custom/token-usage.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { TokenUsage } from "./token-usage";
+
+const meta = {
+    title: "Custom/TokenUsage",
+    component: TokenUsage,
+    args: {
+        inputTokens: {
+            value: 100,
+            label: "100",
+            color: "#A420F6",
+        },
+        outputTokens: {
+            value: 200,
+            label: "200",
+            color: "#C979FF",
+        },
+        totalTokens: {
+            value: 300,
+            label: "300",
+            color: "#DFA6FF",
+        },
+        bgColor: "#F7F9F9",
+    },
+} satisfies Meta<typeof TokenUsage>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        inputTokens: {
+            value: 100,
+            label: "100",
+            color: "#A420F6",
+        },
+        outputTokens: {
+            value: 200,
+            label: "200",
+            color: "#C979FF",
+        },
+        totalTokens: {
+            value: 300,
+            label: "300",
+            color: "#DFA6FF",
+        },
+        bgColor: "#F7F9F9",
+    },
+};
---
+++ lib/components/custom/token-usage.tsx
@@ -0,0 +1,67 @@
+import { Progress } from "./progress";
+
+interface TokenUsageProps {
+    inputTokens: {
+        value: number;
+        label: string;
+        color: string;
+    };
+    outputTokens: {
+        value: number;
+        label: string;
+        color: string;
+    };
+    totalTokens: {
+        value: number;
+        label: string;
+        color: string;
+    };
+    bgColor: string;
+}
+
+export function TokenUsage({ inputTokens, outputTokens, totalTokens, bgColor = "#F7F9F9" }: TokenUsageProps) {
+    return (
+        <div>
+            <ul className="grid gap-2">
+                <li>
+                    <Progress
+                        value={inputTokens.value / totalTokens.value * 100}
+                        bgColor={bgColor}
+                        activeColor={inputTokens.color}
+                    />
+                </li>
+                <li>
+                    <Progress
+                        value={outputTokens.value / totalTokens.value * 100}
+                        bgColor={bgColor}
+                        activeColor={outputTokens.color}
+                    />
+                </li>
+                <li>
+                    <Progress
+                        value={totalTokens.value / totalTokens.value * 100}
+                        bgColor={bgColor}
+                        activeColor={totalTokens.color}
+                    />
+                </li>
+            </ul>
+            <ul className="grid gap-1 mt-4 text-xs whitespace-nowrap">
+                <li className="flex gap-1 items-center">
+                    <span className="block w-2 h-2 rounded-full" style={{ backgroundColor: inputTokens.color }}></span>
+                    <span className="font-medium">{inputTokens.label}</span>
+                    <span className="text-muted-foreground">Prompt tokens</span>
+                </li>
+                <li className="flex gap-1 items-center">
+                    <span className="block w-2 h-2 rounded-full" style={{ backgroundColor: outputTokens.color }}></span>
+                    <span className="font-medium">{outputTokens.label}</span>
+                    <span className="text-muted-foreground">Completion tokens</span>
+                </li>
+                <li className="flex gap-1 items-center">
+                    <span className="block w-2 h-2 rounded-full" style={{ backgroundColor: totalTokens.color }}></span>
+                    <span className="font-medium">{totalTokens.label}</span>
+                    <span className="text-muted-foreground">Usage tokens</span>
+                </li>
+            </ul>
+        </div>
+    );
+}
