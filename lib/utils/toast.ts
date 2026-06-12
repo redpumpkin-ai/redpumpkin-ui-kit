a326e816d1b0bdd73cd24829276b3d45b70fe5b7
Fachri Riyanto <fachri@redpumpkin.ai>
2026-06-12 13:29:09 +0700

    update: add toast utility class names and increment version to 0.0.5

---
+++ lib/utils/toast.ts
@@ -0,0 +1,6 @@
+export const toastClassNames = {
+    success: "!bg-green-50 !text-green-800 !border-green-100",
+    error: "!bg-red-50 !text-red-800 !border-red-100",
+    warning: "!bg-yellow-50 !text-yellow-800 !border-yellow-100",
+    info: "!bg-blue-50 !text-blue-800 !border-blue-100",
+};
